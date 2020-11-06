7ace77db87b9d40e89ee1f62c5c826a3b95543db
Geoffrey <11151445+g3offrey@users.noreply.github.com>
2020-11-07 00:18:38 +0100

    use environment variables for verseion configuration

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+ARCHITECTURE=${ARCH:="linux-amd64"}
+PROMETHEUS_VERSION=${PROM_VERSION:="2.22.1"}
+NODE_EXPORTER_VERSION=${NE_VERSION:="1.0.1"}
+
+PROMETHEUS_RELEASE=prometheus-$PROMETHEUS_VERSION.$ARCHITECTURE
+NODE_EXPORTER_RELEASE=node_exporter-$NODE_EXPORTER_VERSION.$ARCHITECTURE
+
 useradd --no-create-home --shell /bin/false prometheus
 useradd --no-create-home --shell /bin/false node_exporter
 
@@ -9,17 +16,17 @@ mkdir /var/lib/prometheus
 chown prometheus:prometheus /etc/prometheus
 chown prometheus:prometheus /var/lib/prometheus
 
-wget https://github.com/prometheus/prometheus/releases/download/v2.22.1/prometheus-2.22.1.linux-amd64.tar.gz
-tar xvf ./prometheus-2.22.1.linux-amd64.tar.gz
+wget https://github.com/prometheus/prometheus/releases/download/v$PROMETHEUS_VERSION/$PROMETHEUS_RELEASE.tar.gz
+tar xvf ./$PROMETHEUS_RELEASE.tar.gz
 
-cp ./prometheus-2.22.1.linux-amd64/prometheus /usr/local/bin/
-cp ./prometheus-2.22.1.linux-amd64/promtool /usr/local/bin/
+cp ./$PROMETHEUS_RELEASE/prometheus /usr/local/bin/
+cp ./$PROMETHEUS_RELEASE/promtool /usr/local/bin/
 
 chown prometheus:prometheus /usr/local/bin/prometheus
 chown prometheus:prometheus /usr/local/bin/promtool
 
-cp -r ./prometheus-2.22.1.linux-amd64/consoles /etc/prometheus
-cp -r ./prometheus-2.22.1.linux-amd64/console_libraries /etc/prometheus
+cp -r ./$PROMETHEUS_RELEASE/consoles /etc/prometheus
+cp -r ./$PROMETHEUS_RELEASE/console_libraries /etc/prometheus
 
 chown -R prometheus:prometheus /etc/prometheus/consoles
 chown -R prometheus:prometheus /etc/prometheus/console_libraries
@@ -32,10 +39,10 @@ systemctl daemon-reload
 systemctl start prometheus
 systemctl enable prometheus
 
-wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
-tar xvf ./node_exporter-1.0.1.linux-amd64.tar.gz
+wget https://github.com/prometheus/node_exporter/releases/download/v$NODE_EXPORTER_VERSION/$NODE_EXPORTER_RELEASE.tar.gz
+tar xvf ./$NODE_EXPORTER_RELEASE.tar.gz
 
-cp ./node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin
+cp ./$NODE_EXPORTER_RELEASE/node_exporter /usr/local/bin
 
 chown node_exporter:node_exporter /usr/local/bin/node_exporter
 
